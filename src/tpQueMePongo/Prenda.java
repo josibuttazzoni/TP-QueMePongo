bdcfa8962f9a42328224d8d9550d52b876f0bb4a
Josi Buttazzoni <mariajosebuttazzoni@hotmail.com>
2020-04-08 15:22:19 -0300

    Commit inicial

---
+++ src/tpQueMePongo/Prenda.java
@@ -0,0 +1,24 @@
+package tpQueMePongo;
+
+public class Prenda {
+	
+	Tipo tipo;
+	Categoria categoria;
+	String material;
+	Color color;
+	
+	
+	
+	
+}
+
+
+enum Tipo {
+	zapatos, camisaMangaCorta, pantalon;
+}
+
+enum Categoria {
+	parteSuperior, calzado, parteInferior, accesorios;
+}
+
+enum Color {}
