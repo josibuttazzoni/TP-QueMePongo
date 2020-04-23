0c8679fc1f25d62e12678b7d8262de9b1f988788
Josi Buttazzoni <mariajosebuttazzoni@hotmail.com>
2020-04-22 22:06:56 -0300

    Agrego clase abstracta Fabricante

---
+++ src/tpQueMePongo/Fabricante.java
@@ -0,0 +1,10 @@
+package tpQueMePongo;
+
+public abstract class Fabricante {
+
+	void configurarParteSuperior() {}
+	void configurarParteInferior() {}
+	void configurarCalzado() {}
+	void fabricarUniforme() {}
+	
+}
