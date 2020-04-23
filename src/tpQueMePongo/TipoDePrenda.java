2e3daed4ee6a1a27586b5e181288b3af2730f2b4
Josi Buttazzoni <mariajosebuttazzoni@hotmail.com>
2020-04-22 22:10:58 -0300

    Agrego constructor categoria

--- src/tpQueMePongo/TipoDePrenda.java
+++ src/tpQueMePongo/TipoDePrenda.java
@@ -3,6 +3,10 @@ package tpQueMePongo;
 public class TipoDePrenda {
 	Categoria categoria;
 	
+	public TipoDePrenda (Categoria categoria) {
+		this.categoria = categoria;
+	}
+	
 	public Categoria getCategoria() {
 		return categoria;
 	}
